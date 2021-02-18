84b59af767ba84f914d5a214c2a78778886e68bd
Goku-kun <jethvadharmarajsinh@gmail.com>
2021-02-18 12:11:33 +0530

    ADD: version 0.1.1 & UPDATE: README.md

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # boilerplatenode
-This is a node cli application intended for generating the starter code for any node file, module or package.
+
+> This is a node cli application intended for generating the starter code for any node file, module or package.
+
+## Table of contents
+
+* [General info](#general-info)
+* [Screenshots](#screenshots)
+* [Technologies](#technologies)
+* [Setup](#setup)
+* [Features](#features)
+* [Status](#status)
+* [Inspiration](#inspiration)
+* [Contact](#contact)
+
+## General info
+
+Instead of having to create .js files and having to import same utilities time and again, let `boilerplatenode` handle this work for you. Just run it as a regular command after linking it and pass the appropriate flags for importing these system modules. `boilerplatenode` automates this work. You can take this further and even write shell scripts calling this command and it would generate as many files as you want.
+
+It doesn't have any dependencies and hence it'll work with a system that has node installed without requiring anything else.
+
+## Screenshots
+<!-- ![Example screenshot](./img/screenshot.png) -->
+
+## Technologies
+
+* NodeJS - version 14.15.0
+* Windows Terminal
+* Bash shell
+
+## Setup
+
+<!-- TODO: -->
+
+## Features
+<!-- List of features ready and TODOs for future development
+* boilerplate feature
+* boilerplate feature
+* boilerplate feature -->
+
+To-do list:
+
+* ADD functionality for passing filename as option in the shell environment.
+* Write unit tests for the functionality.
+* Make it more flexible by passing number of files to be generated.
+
+## Status
+
+Project is: _in progress_ 
+
+
+## Inspiration
+
+The sole inspiration for creating this package is to automate the daily task of creating files using `touch` and then importing the same boiler plate code everyday before starting to write some actual implementation of the new program.
+
+## Contact
+
+Created by [@Goku-kun](https://www.github.com/Goku-kun) - feel free to contact me!
+
+## License
+
+All code and documentation are released under GNU GENERAL PUBLIC LICENSE and a copy of [license](./LICENSE) is also included in the repository.
---
+++ main.js
@@ -0,0 +1,83 @@
+#! /usr/bin/env node
+
+const args = process.argv.slice(2);
+const fs = require('fs');
+const path = require('path');
+
+(function() {
+    if (args.includes('--help') || (args.includes('-h'))) {
+        printHelp();
+        process.exit(0);
+    }
+})();
+
+let boilterPlateCode =
+    `#! /usr/bin/env node
+
+"use strict";
+
+`;
+
+// BASE_PATH = path.resolve(
+//     process.env.BASE_PATH ||
+//     __dirname
+// );
+// console.log(BASE_PATH);
+
+
+
+
+// ***********************************
+
+
+function printHelp() {
+    console.log("Boiler Plate Node usage:");
+    console.log("");
+    console.log("        boilerplatenode --file=[NAME] [option1, option2,...] ");
+    console.log("");
+    console.log("Options:");
+    console.log("-h, --help             to print this help");
+    console.log("-f, --fs               to require the fs module in the index.js file");
+    console.log("-p, --path             to require the path module in the index.js file");
+    console.log("-o, --os               to require the os module in the index.js file");
+    console.log("-u, --util             to require the util module in the index.js file");
+    console.log("-z, --zlib             to require the zlib module in the index.js file");
+    console.log("-u, --url              to require the url module in the index.js file");
+    console.log("");
+    console.log("");
+    console.log("Defaults:");
+    console.log("If the --file=[name] is not specified, the default value of file would be index.js");
+    console.log("If no options are specified, just the shebang for execution will be written and strict mode will be enabled in the file.");
+    console.log("");
+    console.log("Example:");
+    console.log("");
+    console.log("$boilerplatenode --file=main --fs");
+    console.log("");
+    console.log("This command will create a file called main.js with required fs module.");
+    console.log("");
+    console.log("");
+    // --os, --path, --fs, --util, --zlib, --url
+}
+
+
+(function main() {
+    if (args.includes('-f') || args.includes('--fs')) {
+        boilterPlateCode += "const fs = require('fs');\n";
+    }
+    if (args.includes('--path') || args.includes('-p')) {
+        boilterPlateCode += "const path = require('path');\n";
+    }
+    if (args.includes('-o') || args.includes('--os')) {
+        boilterPlateCode += "const os = require('os');\n";
+    }
+    if (args.includes('-u') || args.includes('--util')) {
+        boilterPlateCode += "const util = require('util');\n";
+    }
+    if (args.includes('-z') || args.includes('--zlib')) {
+        boilterPlateCode += "const zlib = require('zlib');\n";
+    }
+    if (args.includes('-u') || args.includes('--url')) {
+        boilterPlateCode += "const url = require('url');\n";
+    }
+    fs.writeFileSync('./index.js', boilterPlateCode, { encoding: 'utf-8' });
+})();
